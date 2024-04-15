6ce20cc0cba1d4ee980db31e34cdf50ce9dba3e2
laurengustafson <gustaflc@bc.edu>
2024-04-15 15:41:39 -0400

    new changes

---
+++ finalproject.m
@@ -0,0 +1,32 @@
+%Final Project Lauren, Manica, Anshika 
+%4/14/2024
+
+%data for US, China, India, UK, Brazil, Egypt 
+
+%% 
+%loading in CO2 emissions data 
+
+CO2data=table2array(exportemissions);
+
+%delete first two nan rows 
+CO2data(1:2, :) = [];
+
+years=CO2data(:,1);
+BrazilCO2=CO2data(:,2);
+ChinaCO2=CO2data(:,3);
+EgyptCO2=CO2data(:,4);
+IndiaCO2=CO2data(:,5);
+UKCO2=CO2data(:,6);
+USCO2=CO2data(:,7);
+
+figure, clf
+plot(years, BrazilCO2, 'Color', 'g')
+hold on 
+plot(years, ChinaCO2, 'Color', 'r')
+plot(years, USCO2, 'Color','m')
+plot(years, IndiaCO2, 'Color', 'b')
+plot(years, UKCO2, 'Color', 'c')
+plot(years, EgyptCO2, 'Color', 'y')
+xlabel('Years')
+ylabel('emissions (label?)')
+title('CO2 Emissions for Six Different Countries')
