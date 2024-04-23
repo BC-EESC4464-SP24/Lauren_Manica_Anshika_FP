0c8c882137ef30c5010985e05092fa93b2681915
Mnawaz11 <nawazm@bc.edu>
2024-04-23 15:34:04 -0400

    hi

--- Population_data.m
+++ Population_data.m
@@ -10,12 +10,12 @@ India = POP(7,2:64);
 
 hold on;
 figure(1); 
-plot(Years, China, 'r', 'DisplayName', 'China');
-plot(Years, Egypt, 'k', 'DisplayName', 'Egypt');
-plot(Years, UK, 'm', 'DisplayName', 'UK');
-plot(Years, US, 'b', 'DisplayName', 'US');
-plot(Years, India, 'g', 'DisplayName', 'India');
-
+plot(Years, China, 'r', 'DisplayName', 'China','LineWidth', 2);
+plot(Years, Egypt, 'k', 'DisplayName', 'Egypt','LineWidth', 2);
+plot(Years, UK, 'm', 'DisplayName', 'UK','LineWidth', 2);
+plot(Years, US, 'b', 'DisplayName', 'US','LineWidth', 2);
+plot(Years, India, 'g', 'DisplayName', 'India','LineWidth', 2);
+plot(Years, Brazil, 'c', 'DisplayName', 'Brazil','LineWidth', 2);
 grid on 
 
 ax = gca;
@@ -25,5 +25,37 @@ xlabel('Year');
 ylabel('Population');
 title('Population Over Time');
 legend('show');
+%% 
 
+figure(2); 
+
+subplot(2,1,1);
+title('Developed Countries Population');
+hold on
+plot(Years, China, 'r', 'DisplayName', 'China','LineWidth', 2);
+plot(Years, UK, 'm', 'DisplayName', 'UK','LineWidth', 2);
+plot(Years, US, 'b', 'DisplayName', 'US','LineWidth', 2);
+ax = gca;
+ax.YAxis.Exponent = 0
+ax.YAxis.TickLabelFormat = '%,.0f';
+legend('show');
+grid on
+
+
+subplot(2,1,2);
+title('Developing Countries Population');
+hold on
+plot(Years, India, 'g', 'DisplayName', 'India','LineWidth', 2);
+plot(Years, Brazil, 'c', 'DisplayName', 'Brazil','LineWidth', 2);
+plot(Years, Egypt, 'k', 'DisplayName', 'Egypt','LineWidth', 2);
+ax = gca;
+ax.YAxis.Exponent = 0
+ax.YAxis.TickLabelFormat = '%,.0f';
+legend('show');
+grid on
+
+%% World Map
+
+plot(worldmap);
+load coastlines
 
