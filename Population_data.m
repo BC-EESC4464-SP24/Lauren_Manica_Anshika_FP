3c7cc33cc4d161fff44143fa94d0eeb3625f11a7
Mnawaz11 <nawazm@bc.edu>
2024-04-23 11:25:35 -0400

    pushing p

--- Population_data.m
+++ Population_data.m
@@ -9,14 +9,21 @@ US = POP(6,2:64);
 India = POP(7,2:64);
 
 hold on;
-
+figure(1); 
 plot(Years, China, 'r', 'DisplayName', 'China');
 plot(Years, Egypt, 'k', 'DisplayName', 'Egypt');
 plot(Years, UK, 'm', 'DisplayName', 'UK');
 plot(Years, US, 'b', 'DisplayName', 'US');
 plot(Years, India, 'g', 'DisplayName', 'India');
 
+grid on 
+
+ax = gca;
+ax.YAxis.Exponent = 0
+ax.YAxis.TickLabelFormat = '%,.0f';
 xlabel('Year');
 ylabel('Population');
 title('Population Over Time');
 legend('show');
+
+
