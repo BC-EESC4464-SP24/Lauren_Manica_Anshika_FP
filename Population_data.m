35c4bd52f0cf5dd22db215948694325ff787fb5c
Mnawaz11 <nawazm@bc.edu>
2024-04-23 21:59:14 -0400

    jk

--- Population_data.m
+++ Population_data.m
@@ -10,12 +10,12 @@ India = POP(7,2:64);
 
 hold on;
 figure(1); 
-plot(Years, China, 'r', 'DisplayName', 'China','LineWidth', 2);
-plot(Years, Egypt, 'k', 'DisplayName', 'Egypt','LineWidth', 2);
-plot(Years, UK, 'm', 'DisplayName', 'UK','LineWidth', 2);
-plot(Years, US, 'b', 'DisplayName', 'US','LineWidth', 2);
-plot(Years, India, 'g', 'DisplayName', 'India','LineWidth', 2);
-plot(Years, Brazil, 'c', 'DisplayName', 'Brazil','LineWidth', 2);
+plot(Years, China,  'DisplayName', 'China','LineWidth', 2);
+plot(Years, Egypt, 'DisplayName', 'Egypt','LineWidth', 2);
+plot(Years, UK, 'DisplayName', 'UK','LineWidth', 2);
+plot(Years, US, 'DisplayName', 'US','LineWidth', 2);
+plot(Years, India, 'DisplayName', 'India','LineWidth', 2);
+plot(Years, Brazil, 'DisplayName', 'Brazil','LineWidth', 2);
 grid on 
 
 ax = gca;
@@ -32,9 +32,9 @@ figure(2);
 subplot(2,1,1);
 title('Developed Countries Population');
 hold on
-plot(Years, China, 'r', 'DisplayName', 'China','LineWidth', 2);
-plot(Years, UK, 'm', 'DisplayName', 'UK','LineWidth', 2);
-plot(Years, US, 'b', 'DisplayName', 'US','LineWidth', 2);
+plot(Years, China, 'DisplayName', 'China','LineWidth', 2);
+plot(Years, UK, 'DisplayName', 'UK','LineWidth', 2);
+plot(Years, US, 'DisplayName', 'US','LineWidth', 2);
 ax = gca;
 ax.YAxis.Exponent = 0
 ax.YAxis.TickLabelFormat = '%,.0f';
@@ -45,9 +45,9 @@ grid on
 subplot(2,1,2);
 title('Developing Countries Population');
 hold on
-plot(Years, India, 'g', 'DisplayName', 'India','LineWidth', 2);
-plot(Years, Brazil, 'c', 'DisplayName', 'Brazil','LineWidth', 2);
-plot(Years, Egypt, 'k', 'DisplayName', 'Egypt','LineWidth', 2);
+plot(Years, India, 'DisplayName', 'India','LineWidth', 2);
+plot(Years, Brazil, 'DisplayName', 'Brazil','LineWidth', 2);
+plot(Years, Egypt', 'DisplayName', 'Egypt','LineWidth', 2);
 ax = gca;
 ax.YAxis.Exponent = 0
 ax.YAxis.TickLabelFormat = '%,.0f';
@@ -55,7 +55,21 @@ legend('show');
 grid on
 
 %% World Map
+% Load coastlines data
+addpath(genpath('C:\Users\nawazm\Documents\GitHub\Lauren_Manica_Anshika_FP\EDEA Final Project Renewable Data'));
+figure;
+worldmap('World');
+load coastlines;
+hold on
 
-plot(worldmap);
-load coastlines
+
+
+country = shaperead('gadm41_EGY_0.shp'); % Read shape file
+[X, Y] = reducem(country.X',country.Y',0.01); % Play with tolerance to your liking
+ % Make a new figure
+geoshow(Y,X,'displaytype','polygon'); % Plot it up, just like above, only faster (lower resolution)
+axis off; % Style preference, turn off plot axis and just show country
+geoshow(coastlat, coastlon, 'DisplayType', 'line');
+% egp = shaperead('gadm41_EGY_0.shp')
+
 
